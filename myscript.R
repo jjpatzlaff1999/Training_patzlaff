855490769c03b06af936cd3388a4b47f0ed5e071
jjpatzlaff1999 <jessica.patzlaff@deltacouncil.ca.gov>
2026-04-07 14:41:30 -0700

    Update gitignore file

--- myscript.R
+++ myscript.R
@@ -9,6 +9,5 @@ usethis::use_git_config(user.name = "jjpatzlaff1999",
 usethis::git_default_branch_configure(name = "main")
 usethis::git_sitrep()
 usethis::create_github_token()
-
 gitcreds::gitcreds_set()
 usethis::git_sitrep()
